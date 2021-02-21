2d6efca008580fa70cc0e24f4c65114f7c440424
Dementor2k18 <akshaysh6@gmail.com>
2021-02-21 17:08:29 -0300

    error

--- admin/includes/errors.php
+++ admin/includes/errors.php
@@ -1,40 +1,11 @@
 <?php
-<<<<<<< HEAD
+// <<<<<<< HEAD
 if (isset($_SESSION['error'])) {
-  // echo "
-  //     <div class='alert alert-danger alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
-  //       <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-  //       <h3><i class='icon fa fa-warning' style ='color: white;></i> Error!</h3>
-  //       " . $_SESSION['error'] . "
-  //     </div>
-  //   ";
-  unset($_SESSION['error']);
-}
-if (isset($_SESSION['success'])) {
-  // echo "
-  //   <div class='alert alert-success alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
-  //       <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-  //       <h3><i class='icon fa fa-check' style ='color: white;></i> Toegevoegd</h3>
-  //       " . $_SESSION['success'] . "
-  //     </div>
-  //   ";
-  unset($_SESSION['success']);
-}
-if (isset($_SESSION['warning'])) {
-  // echo "
-  //     <div class='alert alert-warning alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
-  //       <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-  //       <h3><i class='icon fa fa-check' style ='color: white;></i> Deleted!</h3>
-  //       " . $_SESSION['warning'] . "
-  //     </div>
-  //   ";
-=======
-if (isset($_SESSION["error"])) {
   echo "
       <div class='alert alert-danger alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-        <h3><i class='icon fa fa-warning' style ='color: white;'></i> Error!</h3>
-        " . $_SESSION["error"] . "
+        <h3><i class='icon fa fa-warning' style ='color: white;></i> Error!</h3>
+        " . $_SESSION['error'] . "
       </div>
     ";
   unset($_SESSION['error']);
@@ -43,7 +14,7 @@ if (isset($_SESSION['success'])) {
   echo "
     <div class='alert alert-success alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-        <h3><i class='icon fa fa-check' style ='color: white;'></i> Toegevoegd</h3>
+        <h3><i class='icon fa fa-check' style ='color: white;></i> Toegevoegd</h3>
         " . $_SESSION['success'] . "
       </div>
     ";
@@ -53,10 +24,40 @@ if (isset($_SESSION['warning'])) {
   echo "
       <div class='alert alert-warning alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-        <h3><i class='icon fa fa-check' style ='color: white;'></i> Deleted!</h3>
+        <h3><i class='icon fa fa-check' style ='color: white;></i> Deleted!</h3>
         " . $_SESSION['warning'] . "
       </div>
     ";
->>>>>>> bed1578ce390468b56f7e293f957b2d24c9eaa55
-  unset($_SESSION['warning']);
 }
+// =======
+// if (isset($_SESSION["error"])) {
+//   echo "
+//       <div class='alert alert-danger alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
+//         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+//         <h3><i class='icon fa fa-warning' style ='color: white;'></i> Error!</h3>
+//         " . $_SESSION["error"] . "
+//       </div>
+//     ";
+//   unset($_SESSION['error']);
+// }
+// if (isset($_SESSION['success'])) {
+//   echo "
+//     <div class='alert alert-success alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
+//         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+//         <h3><i class='icon fa fa-check' style ='color: white;'></i> Toegevoegd</h3>
+//         " . $_SESSION['success'] . "
+//       </div>
+//     ";
+//   unset($_SESSION['success']);
+// }
+// if (isset($_SESSION['warning'])) {
+//   echo "
+//       <div class='alert alert-warning alert-dismissible' role='alert' style ='margin-top:45%; margin-left:85%;'>
+//         <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+//         <h3><i class='icon fa fa-check' style ='color: white;'></i> Deleted!</h3>
+//         " . $_SESSION['warning'] . "
+//       </div>
+//     ";
+// >>>>>>> bed1578ce390468b56f7e293f957b2d24c9eaa55
+//   unset($_SESSION['warning']);
+// }
